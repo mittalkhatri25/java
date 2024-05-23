0453d7b98cd1c1d192d5444284440f57773220bc
mittalkhatri25 <150047330+mittalkhatri25@users.noreply.github.com>
2024-05-23 20:37:19 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,13 @@
+package geometry;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ GeometryPackageDemo.java
@@ -0,0 +1,15 @@
+import Circle;
+import Rectangle;
+import Triangle;
+
+public class GeometryPackageDemo {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        Triangle triangle = new Triangle(3.0, 4.0, 5.0);
+
+        System.out.println("Circle area: " + circle.area());
+        System.out.println("Rectangle area: " + rectangle.area());
+        System.out.println("Triangle area: " + triangle.area());
+    }
+}
